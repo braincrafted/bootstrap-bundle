39f2509f4c2df76051a71ef5e4a62ff7d821cf3d
Florian Eckerstorfer <florian@theroadtojoy.at>
2012-07-23 16:21:58 +0200

    Added support for KnpMenu.

---
+++ Voter/RequestVoter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Braincrafted\BootstrapBundle  \Voter;
+
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Matcher\Voter\VoterInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Voter based on the uri
+ */
+class RequestVoter implements VoterInterface
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Checks whether an item is current.
+     *
+     * If the voter is not able to determine a result,
+     * it should return null to let other voters do the job.
+     *
+     * @param ItemInterface $item
+     * @return boolean|null
+     */
+    public function matchItem(ItemInterface $item)
+    {
+        if ($item->getUri() ===
+            $this->container->get('request')->getRequestUri()) {
+            return true;
+        }
+
+        return null;
+    }
+}
