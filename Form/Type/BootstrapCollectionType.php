7a7fade64c97433000a053264e1bc0f5113e7e5d
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-04-17 17:13:11 +0200

    Configurable button labels in BootstrapCollectionType

--- Form/Type/BootstrapCollectionType.php
+++ Form/Type/BootstrapCollectionType.php
@@ -3,16 +3,57 @@
 namespace Bc\Bundle\BootstrapBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\Extension\Core\EventListener\ResizeFormListener;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class BootstrapCollectionType extends AbstractType
 {
     /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'allow_add'    => $options['allow_add'],
+            'allow_delete' => $options['allow_delete'],
+            'add_button_text'   => $options['add_button_text'],
+            'delete_button_text'    => $options['delete_button_text']
+        ));
+
+        if ($form->getConfig()->hasAttribute('prototype')) {
+            $view->vars['prototype'] = $form->getConfig()->getAttribute('prototype')->createView($view);
+        }
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array());
+        $optionsNormalizer = function (Options $options, $value) {
+            $value['block_name'] = 'entry';
+
+            return $value;
+        };
+
+        $resolver->setDefaults(array(
+            'allow_add'      => false,
+            'allow_delete'   => false,
+            'prototype'      => true,
+            'prototype_name' => '__name__',
+            'type'           => 'text',
+            'add_button_text'   => 'Add',
+            'delete_button_text'    => 'Delete',
+            'options'        => array(),
+        ));
+
+        $resolver->setNormalizers(array(
+            'options' => $optionsNormalizer,
+        ));
     }
 
     /**
