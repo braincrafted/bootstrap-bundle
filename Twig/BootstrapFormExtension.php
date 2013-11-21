5adb21098ad19af828bba06f8f75bf359e2ec4d3
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-11-21 16:12:06 +0100

    Set column size

--- Twig/BootstrapFormExtension.php
+++ Twig/BootstrapFormExtension.php
@@ -21,6 +21,9 @@ class BootstrapFormExtension extends \Twig_Extension
     /** @var string */
     private $style;
 
+    /** @var string */
+    private $colSize = 'lg';
+
     /**
      * {@inheritdoc}
      */
@@ -29,6 +32,8 @@ class BootstrapFormExtension extends \Twig_Extension
         return array(
             new \Twig_SimpleFunction('bootstrap_set_style', array($this, 'setStyle')),
             new \Twig_SimpleFunction('bootstrap_get_style', array($this, 'getStyle')),
+            new \Twig_SimpleFunction('bootstrap_set_col_size', array($this, 'setColSize')),
+            new \Twig_SimpleFunction('bootstrap_get_col_size', array($this, 'getColSize')),
             'checkbox_row'  => new \Twig_Function_Node(
                 'Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode',
                 array('is_safe' => array('html'))
@@ -66,5 +71,25 @@ class BootstrapFormExtension extends \Twig_Extension
     public function getStyle()
     {
         return $this->style;
+    }
+
+    /**
+     * Sets the column size.
+     *
+     * @param string $colSize Column size (xs, sm, md or lg)
+     */
+    public function setColSize($colSize)
+    {
+        $this->colSize = $colSize;
+    }
+
+    /**
+     * Returns the column size.
+     *
+     * @return string Column size (xs, sm, md or lg)
+     */
+    public function getColSize()
+    {
+        return $this->colSize;
     }
 }
