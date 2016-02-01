2d2b1de4cfb5e5427538f217977a7ee2a1be8997
Lars Vierbergen <vierbergenlars@gmail.com>
2016-02-01 13:25:54 +0100

    Add missing import of LegacyFormHelper in FormStaticControlType

--- Form/Type/FormStaticControlType.php
+++ Form/Type/FormStaticControlType.php
@@ -8,6 +8,7 @@ namespace Braincrafted\Bundle\BootstrapBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Braincrafted\Bundle\BootstrapBundle\Util\LegacyFormHelper;
 
 /**
  * FormStaticControlType
